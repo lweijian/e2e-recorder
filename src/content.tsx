8ddd2709c05c32a5509bf8993fa0ad3127ff18f6
wangjiangjian <wangjiangjian.0224@bytedance.com>
2024-04-20 23:03:21 +0800

    feat: format code by prettier

--- src/content.tsx
+++ src/content.tsx
@@ -1,45 +1,38 @@
 import cssText from "data-text:~/style.css"
 import type { PlasmoCSConfig } from "plasmo"
-import SelectorRecorder from "~/selector-recorder";
-import {
-    RecoilRoot,
-    useRecoilValue,
-    useSetRecoilState,
-} from 'recoil';
-import { selectorListAtom } from "~/store";
-import { useEffect } from "react";
+import { useEffect } from "react"
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil"
+
+import SelectorRecorder from "~/selector-recorder"
+import { selectorListAtom } from "~/store"
 
 export const config: PlasmoCSConfig = {
-    matches: ["https://ads.tiktok.com/*"]
+  matches: ["https://ads.tiktok.com/*", "http://127.0.0.1:8080/*"]
 }
 
 export const getStyle = () => {
-    const style = document.createElement("style")
-    style.textContent = cssText
-    return style
+  const style = document.createElement("style")
+  style.textContent = cssText
+  return style
 }
 const RecorderOverlay = () => {
-    const selectorList = useRecoilValue(selectorListAtom);
-    const setSelectorListAtom = useSetRecoilState(selectorListAtom);
-    useEffect(() => {
-        const recorder = new SelectorRecorder(setSelectorListAtom);
-        return () => {
-            recorder.destroy();
-        };
-    }, []);
-    return (
-        <RecoilRoot>
-            <div className="recorder-z-50 recorder-flex recorder-fixed recorder-top-32 recorder-right-8">
-                {
-                    selectorList.map((item) => {
-                        return <div>
-                            selector: {item.selector}
-                        </div>
-                    })
-                }
-            </div>
-        </RecoilRoot>
-    )
+  const selectorList = useRecoilValue(selectorListAtom)
+  const setSelectorListAtom = useSetRecoilState(selectorListAtom)
+  useEffect(() => {
+    const recorder = new SelectorRecorder(setSelectorListAtom)
+    return () => {
+      recorder.destroy()
+    }
+  }, [])
+  return (
+    <RecoilRoot>
+      <div className="recorder-z-50 recorder-flex recorder-fixed recorder-top-32 recorder-right-8">
+        {selectorList.map((item) => {
+          return <div>selector: {item.selector}</div>
+        })}
+      </div>
+    </RecoilRoot>
+  )
 }
 
 export default RecorderOverlay
--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,8 @@
-import { atom } from "recoil";
-import type { TargetNode } from "~/selector-recorder";
+import { atom } from "recoil"
+
+import type { TargetNode } from "~/selector-recorder"
 
 export const selectorListAtom = atom<TargetNode[]>({
-    key: 'selectorList', // unique ID (with respect to other atoms/selectors)
-    default: [],
-});
+  key: "selectorList", // unique ID (with respect to other atoms/selectors)
+  default: []
+})
