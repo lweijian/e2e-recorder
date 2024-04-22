61d9d844eac40cbcfa680fe2b7972638f24472cc
hnustwjj <948065841@qq.com>
2024-04-22 17:15:14 +0800

    feat: draggable

--- src/content.tsx
+++ src/content.tsx
@@ -1,7 +1,10 @@
+import { IconDragDotVertical } from "@arco-design/web-react/icon"
 import cssText from "data-text:~/style.css"
 import type { PlasmoCSConfig } from "plasmo"
 import { useEffect, useMemo, useState } from "react"
 import Draggable from "react-draggable" // The default
+
+import "@arco-design/web-react/dist/css/arco.css"
 
 import SelectorRecorder from "~/selector-recorder"
 
@@ -31,13 +34,30 @@ const RecorderOverlay = () => {
   }, [])
 
   const { position, onDrag } = useStoragePosition()
-
+  const [disabled, setDisabled] = useState(true)
   return (
-    <Draggable position={position} onDrag={onDrag}>
-      <div className="r-z-50 r-flex r-flex-col r-fixed r-top-32 r-right-8 r-bg-white r-border r-rounded-lg r-p-3 r-min-w-[500px] r-h-[100px] r-overflow-auto r-cursor-move r-shadow-xl">
-        {recorderList.map((item, idx) => {
-          return <div key={idx}>selector: {item.selector}</div>
-        })}
+    <Draggable
+      onDrag={onDrag}
+      disabled={disabled}
+      position={position}
+      bounds={{
+        left: 0,
+        top: 0,
+        right: document.body.clientWidth - 50,
+        bottom: document.body.clientHeight - 50
+      }}>
+      <div className="r-z-50 r-flex r-flex r-flex-start r-fixed r-top-0 r-left-0 r-bg-white r-border r-rounded-lg r-p-3 r-min-w-[500px] r-h-[100px] r-overflow-auto  r-shadow-xl">
+        <IconDragDotVertical
+          className="r-w-[15px] r-h-[15px] r-mt-[3px]	r-cursor-move "
+          // 用捕获事件，交互会比冒泡好（响应更快）
+          onMouseDownCapture={() => setDisabled(false)}
+          onMouseUpCapture={() => setDisabled(true)}
+        />
+        <div className="r-flex-col r-ml-[7px]">
+          {recorderList.map((item, idx) => {
+            return <div key={idx}>selector: {item.selector}</div>
+          })}
+        </div>
       </div>
     </Draggable>
   )
