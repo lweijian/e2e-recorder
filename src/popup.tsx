a7404e3aa7fad9bfaa634de29ee79ebe82c4405d
hnustwjj <948065841@qq.com>
2024-06-06 15:11:02 +0800

    feat: 调整生成代码的逻辑 事件委托

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,27 +1,30 @@
-import { Switch } from "@arco-design/web-react"
-
-import { useStorage } from "@plasmohq/storage/hook"
-
-import { CountButton } from "~/features/count-button"
+import { Input, Switch } from "@arco-design/web-react"
 
 import "~/style.css"
 import "@arco-design/web-react/dist/css/arco.css"
 
-import useStore, { GENERATE_BY_CLASS, SHOW_CONTENT_UI } from "./hooks/useStore"
+import useStorageValue from "./hooks/useStorageValue"
+import useStore, { SHOW_CONTENT_UI, TEMPLATE } from "./hooks/useStore"
 
 function IndexPopup() {
   const [showContent, setShowContent] = useStore(SHOW_CONTENT_UI)
-  const [generateByClass, setGenerageByClass] = useStore(GENERATE_BY_CLASS)
+  const { state, onChange } = useStorageValue(TEMPLATE)
   return (
     <div className="flex-col p-5 items-center justify-center w-[500px]">
       <div>
         展示浮窗 <Switch checked={showContent} onChange={setShowContent} />
       </div>
-      <div>
+      {/* <div>
         如果无法用data-testId定位，是否用点击元素的类生成选择器{" "}
         <Switch checked={generateByClass} onChange={setGenerageByClass} />
+      </div> */}
+      <div>
+        代码模板
+        <div className="text-xs text-gray-500">
+          支持$text,$selector,$idx变量，其中$text为元素的文本，$selector为元素的选择器，$idx为元素在页面中的顺序
+        </div>
+        <Input value={state} onChange={onChange} />
       </div>
-      <CountButton />
     </div>
   )
 }
