c19b6c182c148fb762e65189b817c3d3bc2bc790
hnustwjj <948065841@qq.com>
2024-06-24 17:22:31 +0800

    fix: 移除draggable 受控,监听body resize

--- src/hooks/useStore.ts
+++ src/hooks/useStore.ts
@@ -1,8 +1,6 @@
 import { Storage } from "@plasmohq/storage"
 import { useStorage } from "@plasmohq/storage/hook"
 
-// 缓存上次用户拖拽的ui界面的位置
-export const POSITION = "position"
 // 是否展示ui界面
 export const SHOW_CONTENT_UI = "show_content_ui"
 //  如果无法用data-testId定位，是否用点击元素的类生成选择器{" "}
@@ -13,7 +11,6 @@ export const TEMPLATE = "template"
 export const CONFIG_STORE = new Storage()
 
 export type StorageKey =
-  | typeof POSITION
   | typeof SHOW_CONTENT_UI
   | typeof GENERATE_BY_CLASS
   | typeof TEMPLATE
