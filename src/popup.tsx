3f8882df2a6ebae105f5b44fa2e662a336b3f94a
wjj <948065841@qq.com>
2025-03-13 16:06:01 +0800

    feat: ui

--- src/popup.tsx
+++ src/popup.tsx
@@ -13,28 +13,36 @@ function IndexPopup() {
   const { state, onChange } = useStorageValue(TEMPLATE)
   return (
     <div className="flex-col p-5 items-center justify-center w-[500px]">
-      <div>
-        record <Switch checked={showContent} onChange={setShowContent} />
+      <div className="flex items-center justify-between w-full mb-2">
+        <div className="text-md text-gray-500">代码模板</div>
+        <div className="text-md text-gray-500 flex items-center">
+          启用插件
+          <Switch
+            checked={showContent}
+            className="ml-2 mb-[2px]"
+            onChange={setShowContent}
+          />
+        </div>
       </div>
       {/* <div>
         如果无法用data-testId定位，是否用点击元素的类生成选择器{" "}
         <Switch checked={generateByClass} onChange={setGenerageByClass} />
       </div> */}
       <div>
-        代码模板
-        <div className="text-xs text-gray-500">
-          支持$text,$selector,$idx变量，其中$text为元素的文本，$selector为元素的选择器，$idx为元素在页面中的顺序。
-        </div>
-        <div className="text-xs text-gray-500">
+        <li className="text-xs text-gray-500">
+          支持$text，$selector，$idx变量，其中 $text 为元素的文本，$selector
+          为元素的选择器，$idx 为元素在页面中的顺序。
+        </li>
+        <li className="text-xs text-gray-500">
           {
-            "\n[]内的内容表示可选，如：[text:$text]代表当$text不存在时，不生成这部分代码。"
+            "\n[] 内的内容表示可选，如：[text:$text] 代表当 $text 不存在时，不生成这部分代码。"
           }
-        </div>
-        <div className="text-xs text-gray-500">
+        </li>
+        <li className="text-xs text-gray-500 mb-2">
           {
             "\n不妨试试 this.page.elements({ [text:$text, ]css:$selector }).index($idx);"
           }
-        </div>
+        </li>
         <Input.TextArea
           style={{ height: "100px" }}
           value={state}
